0c4593114f4a76d1e72db9f1acc712eb11e61b59
kestred <kestred@riotcave.com>
2013-10-24 14:43:59 -0600

    dclass: implement parseStruct in the parser

--- DistributedClass/parse.go
+++ DistributedClass/parse.go
@@ -46,7 +46,6 @@ func (p parser) parse(r io.Reader) *File {
 	for p.parseDeclaration() {
 	}
 
-
 	// Create errors if there are any expected identifiers remaining that have not been defined
 	for keyword, firstLine := range p.expectedKeywords {
 		p.errors = append(p.errors, definitionError(keyword, tokenKeyword, firstLine))
@@ -58,9 +57,8 @@ func (p parser) parse(r io.Reader) *File {
 		p.errors = append(p.errors, definitionError(className, tokenDClass, firstLine))
 	}
 
-
 	// If errors exist, print them and return nil
-	if len(p.errors) {
+	if len(p.errors) > 0 {
 		fmt.Fprintf(os.Stderr, "Encountered %d errors while parsing dclass file...\n", len(p.errors))
 
 		// Print max 10 errors...
@@ -69,11 +67,10 @@ func (p parser) parse(r io.Reader) *File {
 		}
 
 		// ... and mention how many errors were not printed.
-		if(len(p.errors) > 10) {
-			fmt.Fprintf(os.Stderr, "... an extra %d errors were not printed.\n", len(p.errors-10))
+		if len(p.errors) > 10 {
+			fmt.Fprintf(os.Stderr, "... an extra %d errors were not printed.\n", len(p.errors)-10)
 		}
 	}
-
 
 	return p.dcf
 }
@@ -93,9 +90,13 @@ func (p parser) parseDeclaration() bool {
 		} else if t.val == tokenName[tokenStruct] {
 			return p.parseStruct()
 		} else if t.val == tokenName[tokenDClass] {
-			return p.parseDClass()
+			return p.parseClass()
 		}
 		fallthrough
+	case tokenLeftCurly:
+		p.errors = append(p.errors, parseError("expected a declaration but got '"+t.String()+"'",
+			p.lex.lineNumber()))
+		return p.expectRightCurly(p.lex.lineNumber())
 	default:
 		p.errors = append(p.errors, parseError("expected a declaration but got '"+t.String()+"'",
 			p.lex.lineNumber()))
@@ -122,7 +123,7 @@ func (p parser) parseKeyword() bool {
 	default:
 		p.errors = append(p.errors, parseError("unexpected '"+t.String()+"' in 'keyword' declaration",
 			p.lex.lineNumber()))
-		return true
+		return p.expectEndline(p.lex.lineNumber())
 	}
 }
 
@@ -132,30 +133,95 @@ func (p parser) parseStruct() bool {
 	t := p.lex.nextToken()
 	switch t.typ {
 	case tokenEOF:
-		p.errors = append
+		p.errors = append(p.errors, parseError("incomplete 'struct' declaration, found EOF",
+			p.lex.lineNumber()))
+		return false
+	case tokenError:
+		p.errors = append(p.errors, lexError(t, p.lex.lineNumber()))
+		return false
+	case tokenLeftCurly:
+		errStr := "incomplete 'struct' declaration, missing identifier before definition start '{'"
+		p.errors = append(p.errors, parseError(errStr, p.lex.lineNumber()))
+		return p.expectRightCurly(p.lex.lineNumber())
+	case tokenIdentifier:
+		if p.dcf.Structs[t.val] != nil {
+			p.errors = append(p.errors, parseError("struct "+t.val+" already defined above", p.lex.lineNumber()))
+			return p.expectRightCurly(p.lex.lineNumber())
+		}
+
+		return p.parseStructInner(p.dcf.NewStruct(t.val))
+	default:
+		p.errors = append(p.errors, parseError("unexpected '"+t.String()+"' in 'struct' declaration",
+			p.lex.lineNumber()))
+		return true
 	}
+}
+
+// parses the inner struct definition given within a block '{...}'
+func (p parser) parseStructInner(s *Struct) bool {
+	// expect a left curly to open the definition block
+	t := p.lex.nextToken()
+	switch t.typ {
+	case tokenEOF:
+		p.errors = append(p.errors, parseError("incomplete 'struct' declaration, found EOF",
+			p.lex.lineNumber()))
+		return false
+	case tokenError:
+		p.errors = append(p.errors, lexError(t, p.lex.lineNumber()))
+		return false
+	case tokenLeftCurly:
+		break
+	default:
+		p.errors = append(p.errors, parseError("missing '{' after 'struct' declaration, found '"+t.String()+"'",
+			p.lex.lineNumber()))
+		return true
+	}
+
+	// parse for parameters till we find a RightCurly
+	t = p.lex.nextToken()
+	for t.typ != tokenRightCurly && t.typ != tokenEOF && t.typ != tokenError {
+		if(!p.parseField(t)) {
+			return false
+		}
+		t = p.lex.nextToken()
+	}
+
+	// finished struct definition, handle any errors then expect endline
+	switch t.typ {
+	case tokenEOF:
+		p.errors = append(p.errors, parseError("incomplete 'struct' definition, found EOF",
+			p.lex.lineNumber()))
+		return false
+	case tokenError:
+		p.errors = append(p.errors, lexError(t, p.lex.lineNumber()))
+		return false
+	}
+
+	return p.expectEndline(p.lex.lineNumber())
 }
 
 // parses a dclass declaration `dclass foo {...};`, assumes the struct token has already been consumed
 // returns false if upon reaching tokenEOF or tokenError
 // TODO: Implement
-func (p parser) parseDClass() bool {
+func (p parser) parseClass() bool {
 	return true
 }
 
-// parses a parameter
-// returns false if upon reaching tokenEOF or tokenError
+// parses a field, the first token should have been consumed and passed as an argument
+// returns false upon reaching tokenEOF or tokenError
 // TODO: Implement
-func (p parser) parseParameter() bool {
+func (p parser) parseField(firstToken token) bool {
 	return true
 }
 
 func (p parser) expectEndline(startline int) bool {
-	fail := false
+	var fail, next bool
+
+	next = true
 	t := p.lex.nextToken()
 	for t.typ != tokenEndline && t.typ != tokenEOF && t.typ != tokenError {
-		fail = true
 		t = p.lex.nextToken()
+		next = false
 	} // consume all tokens till endline
 
 	switch t.typ {
@@ -166,8 +232,31 @@ func (p parser) expectEndline(startline int) bool {
 		fail = true
 	}
 
-	if fail {
+	if next || fail {
 		p.errors = append(p.errors, parseError("missing semicolon (;) at end of statement", startline))
+	}
+
+	return !fail
+}
+
+func (p parser) expectRightCurly(leftline int) bool {
+	t := p.lex.nextToken()
+	for t.typ != tokenRightCurly && t.typ != tokenEOF && t.typ != tokenError {
+		t = p.lex.nextToken()
+	} // consume all tokens till RightCurly
+
+	fail := false
+	switch t.typ {
+	case tokenEOF:
+		fail = true
+	case tokenError:
+		p.errors = append(p.errors, lexError(t, p.lex.lineNumber()))
+		fail = true
+	}
+
+	if fail {
+		errStr := fmt.Sprintf("missing closing curly brace (}) at end of block starting on line %d", leftline)
+		p.errors = append(p.errors, parseError(errStr, p.lex.lineNumber()))
 	}
 
 	return !fail
@@ -197,7 +286,7 @@ func parseError(msg string, line int) Error {
 }
 
 func definitionError(identifier string, typ tokenType, firstUsed int) Error {
-	errStr := fmt.Sprintf("used %s '%s', but '%s' was never defined", tokenName[typ], keyword, keyword)
-	errStr += fmt.Sprintf("\n\t first used on line: %d", firstLine)
+	errStr := fmt.Sprintf("used %s '%s', but '%s' was never defined", tokenName[typ], identifier, identifier)
+	errStr += fmt.Sprintf("\n\t first used on line: %d", firstUsed)
 	return Error("definition error: " + errStr)
 }
